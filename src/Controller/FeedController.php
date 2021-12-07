147d9d5b558ae4e8f31768863a5f004453ad204b
romai <romainlerme@hotmail.fr>
2021-12-07 16:03:06 +0100

    affichage post

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,9 @@ class FeedController extends AbstractController
      */
     public function index(): Response
     {
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
         return $this->render('feed/index.html.twig', [
-            'controller_name' => 'FeedController',
+            'posts' => $posts,
         ]);
     }
 
