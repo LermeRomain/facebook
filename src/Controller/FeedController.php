e1eaa76da5f329592168292b7f6f7f241887fe4e
romai <romainlerme@hotmail.fr>
2021-12-07 14:40:42 +0100

    security connect et feed

---
+++ src/Controller/FeedController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FeedController extends AbstractController
+{
+    /**
+     * @Route("/feed", name="feed")
+     */
+    public function index(): Response
+    {
+        return $this->render('feed/index.html.twig', [
+            'controller_name' => 'FeedController',
+        ]);
+    }
+}
