a024102fec61e9a4ca3b019971fb7ae954efaee1
romai <romainlerme@hotmail.fr>
2021-12-07 14:53:16 +0100

    Logout

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -17,4 +17,12 @@ class FeedController extends AbstractController
             'controller_name' => 'FeedController',
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
 }
